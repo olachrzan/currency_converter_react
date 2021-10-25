7ffb11d183e5f4fadc0e6565b8033db00f95d5b7
Ola Chrzan <ola.chrzan16@gmail.com>
2021-10-25 18:24:59 +0200

    Add array with currencies data

---
+++ src/currencies.js
@@ -0,0 +1,6 @@
+export const currencies = [
+	{ shortName: "EUR", name: "EUR - euro", rate: 4.60 },
+	{ shortName: "USD", name: "USD - dolar amerykański", rate: 3.95 },
+	{ shortName: "GBP", name: "GBP - funt szterling", rate: 5.44 },
+	{ shortName: "CHF", name: "CHF - frank szwajcarski", rate: 4.31 }
+]
