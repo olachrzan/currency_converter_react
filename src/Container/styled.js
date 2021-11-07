000eb5be7cd3e9f355162779a8f15128b31a0559
Ola Chrzan <ola.chrzan16@gmail.com>
2021-11-07 18:29:11 +0100

    Add styled component to Container

---
+++ src/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  background-color: #fff;
+  max-width: 800px;
+  margin: 100px auto;
+  padding: 20px;
+  padding-bottom: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 10px 20px rgb(48, 45, 15);
+`;
